1a8da64ceaa2dc99d404a43b52c8801ca78fa8cf
sisianteymoorian <89214324+sisianteymoorian@users.noreply.github.com>
2021-12-10 20:17:36 -0800

    modified the file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from threading import *
+import threading
 import time
 import random
 
@@ -6,19 +6,21 @@ n = 110
 k1 = 20
 k2 = 5
 t= 2
-binar = Semaphore()
-buffer = [0 for i in range(n)]
 next_out = 0
 next_in = 0
-
+binar = threading.Semaphore(1)
+buffer = [0 for i in range(n)]
 def Produce(next_in): 
+    binar.acquire()
     while 1:
         for i in range(k1):
             buffer[(next_in + i) % n] += 1
         next_in = (next_in + k1) % n
         t1 = random.randint(0, t)
         time.sleep(t1)
+        binar.release()
 def Consume(next_out):  
+    binar.acquire()
     while 1:
         t2 = random.randint(0, t)
         time.sleep(t2)
@@ -29,9 +31,10 @@ def Consume(next_out):
                 print(buffer)
                 return
         next_out = (next_out + k2) % n
+        binar.release()
 
-Produce = Thread(target = Produce, args=[next_in])
-Consume = Thread(target = Consume, args=[next_out])
+Produce = threading.Thread(target = Produce, args=[next_in])
+Consume = threading.Thread(target = Consume, args=[next_out])
 
 Produce.start()
 Consume.start()
